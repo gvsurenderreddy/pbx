d8a5e66a8df701325cb30d788ca68127db38eae1
aeudes <adrien.eudes@gmail.com>
2014-03-21 17:51:53 +0800

    added extensions

---
+++ src/VoIP/PBX/RealTimeBundle/Form/ExtensionType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace VoIP\PBX\RealTimeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ExtensionType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('context')
+            ->add('exten')
+            ->add('priority')
+            ->add('app')
+            ->add('appdata')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'VoIP\PBX\RealTimeBundle\Entity\Extension'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'voip_pbx_realtimebundle_extension';
+    }
+}
