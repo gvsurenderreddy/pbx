b6290ea47f470dca6f68b51f6848ec4aa6b9e55a
aeudes <adrien.eudes@gmail.com>
2014-05-15 12:33:43 +0800

    mailbox archives

--- src/VoIP/UI/BasicBundle/Controller/CompanyController.php
+++ src/VoIP/UI/BasicBundle/Controller/CompanyController.php
@@ -309,20 +309,37 @@ class CompanyController extends Controller
 		$request = $this->getRequest();
 		$page = $request->query->get('page');
 		if (!$page) $page = 1;
+		$archive = $request->get('archive');
 		$user = $this->getUser();
 		$company = $user->getCompany();
 		$em = $this->getDoctrine()->getManager();
-		$query = $em->createQuery(
-		    'SELECT m
-		    FROM VoIPCompanyVoicemailBundle:Message m
-			LEFT JOIN m.voicemail v
-			LEFT JOIN v.subscription s
-			LEFT JOIN s.company c
-			WHERE c.hash = :hash AND m.archivedAt IS NULL
-			ORDER BY m.createdAt DESC'
-		)->setParameters(array(
-			'hash' => $company->getHash()
-		))->setMaxResults($card)->setFirstResult(($page - 1) * $card);
+		if (!$archive) {
+			$archive = 0;
+			$query = $em->createQuery(
+			    'SELECT m
+			    FROM VoIPCompanyVoicemailBundle:Message m
+				LEFT JOIN m.voicemail v
+				LEFT JOIN v.subscription s
+				LEFT JOIN s.company c
+				WHERE c.hash = :hash AND m.archivedAt IS NULL
+				ORDER BY m.createdAt DESC'
+			)->setParameters(array(
+				'hash' => $company->getHash()
+			))->setMaxResults($card)->setFirstResult(($page - 1) * $card);
+		} else {
+			$query = $em->createQuery(
+			    'SELECT m
+			    FROM VoIPCompanyVoicemailBundle:Message m
+				LEFT JOIN m.voicemail v
+				LEFT JOIN v.subscription s
+				LEFT JOIN s.company c
+				WHERE c.hash = :hash AND m.archivedAt IS NOT NULL
+				ORDER BY m.createdAt DESC'
+			)->setParameters(array(
+				'hash' => $company->getHash()
+			))->setMaxResults($card)->setFirstResult(($page - 1) * $card);
+		}
+		
 
 		$messages = $query->getResult();
 
@@ -330,7 +347,8 @@ class CompanyController extends Controller
         return array(
 			'company' => $company,
 			'messages' => $messages,
-			'page' => $page
+			'page' => $page,
+			'archive' => $archive
 		);
     }
 	
