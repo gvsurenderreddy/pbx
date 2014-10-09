710aca422c14685d3d7e1415e01ed80284170b8a
aeud <adrien.eudes@gmail.com>
2014-10-09 11:17:54 +0800

    New release

--- src/Management/Session/UserBundle/Controller/RegistrationController.php
+++ src/Management/Session/UserBundle/Controller/RegistrationController.php
@@ -14,14 +14,12 @@ namespace Management\Session\UserBundle\Controller;
 use FOS\UserBundle\FOSUserEvents;
 use FOS\UserBundle\Event\FormEvent;
 use FOS\UserBundle\Event\GetResponseUserEvent;
-use FOS\UserBundle\Event\UserEvent;
 use FOS\UserBundle\Event\FilterUserResponseEvent;
-use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\UserBundle\Model\UserInterface;
 use VoIP\Company\StructureBundle\Entity\Company;
 use VoIP\Company\StructureBundle\Entity\Employee;
@@ -36,13 +34,12 @@ class RegistrationController extends Controller
 {
     public function registerAction(Request $request)
     {
-		$em = $this->getDoctrine()->getManager();
         /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
-        $formFactory = $this->container->get('fos_user.registration.form.factory');
+        $formFactory = $this->get('fos_user.registration.form.factory');
         /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
-        $userManager = $this->container->get('fos_user.user_manager');
+        $userManager = $this->get('fos_user.user_manager');
         /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
-        $dispatcher = $this->container->get('event_dispatcher');
+        $dispatcher = $this->get('event_dispatcher');
 
         $user = $userManager->createUser();
         $user->setEnabled(true);
@@ -57,60 +54,59 @@ class RegistrationController extends Controller
         $form = $formFactory->createForm();
         $form->setData($user);
 
-        if ('POST' === $request->getMethod()) {
-            $form->bind($request);
+        $form->handleRequest($request);
 
-            if ($form->isValid()) {
-                $event = new FormEvent($form, $request);
-                $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
-				$user->setUsername($user->getUsername());
+        if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			
+            $event = new FormEvent($form, $request);
+            $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
 
-                $userManager->updateUser($user);
+            $userManager->updateUser($user);
+			
+			$company = new Company();
+			$company->setName($user->getCompanyName());
+			$user->setCompany($company);
+			$outGroup = $em->getRepository('VoIPCompanySubscriptionsBundle:OutGroup')->findOneBy(array(
+				'isPublic' => true
+			), array(
+				'lastAttribution' => 'ASC'
+			));
+			if ($outGroup) {
+				$outGroup->setLastAttribution(new \DateTime());
+				$company->setOutGroup($outGroup);
+			}
+			$company->setCredit(10);
+			$company->setIsMaster(false);
+			$licenseEmployee = $this->container->getParameter('price_employee');
+			$company->setLicenseEmployee($licenseEmployee ? $licenseEmployee : 10);
+			$firstEmployeeLicense = 48;
+			$company->setLicenseFirstEmployee($firstEmployeeLicense);
+			$licenseSubscription = $this->container->getParameter('price_subscription');
+			$company->setLicenseSubscription($licenseSubscription ? $licenseSubscription : 20);
+			$factor = $this->container->getParameter('rate_factor');
+			$company->setRateFactor($factor ? $factor : 2);
+			$em->persist($company);
+			$employee = new Employee();
+			$employee->setIsMain(true);
+			$employee->setName($user->getUserName());
+			$employee->setExtension(100);
+			$employee->setCompany($company);
+			$employee->setLicense($firstEmployeeLicense);
+			$em->persist($employee);
+			$em->flush();
 
-				$company = new Company();
-				$company->setName($user->getCompanyName());
-				$user->setCompany($company);
-				$outGroup = $em->getRepository('VoIPCompanySubscriptionsBundle:OutGroup')->findOneBy(array(
-					'isPublic' => true
-				), array(
-					'lastAttribution' => 'ASC'
-				));
-				if ($outGroup) {
-					$outGroup->setLastAttribution(new \DateTime());
-					$company->setOutGroup($outGroup);
-				}
-				$company->setCredit(10);
-				$company->setIsMaster(false);
-				$licenseEmployee = $this->container->getParameter('price_employee');
-				$company->setLicenseEmployee($licenseEmployee ? $licenseEmployee : 10);
-				$firstEmployeeLicense = 48;
-				$company->setLicenseFirstEmployee($firstEmployeeLicense);
-				$licenseSubscription = $this->container->getParameter('price_subscription');
-				$company->setLicenseSubscription($licenseSubscription ? $licenseSubscription : 20);
-				$factor = $this->container->getParameter('rate_factor');
-				$company->setRateFactor($factor ? $factor : 2);
-				$em->persist($company);
-				$employee = new Employee();
-				$employee->setIsMain(true);
-				$employee->setName($user->getUserName());
-				$employee->setExtension(100);
-				$employee->setCompany($company);
-				$employee->setLicense($firstEmployeeLicense);
-				$em->persist($employee);
-				$em->flush();
-
-                if (null === $response = $event->getResponse()) {
-                    $url = $this->container->get('router')->generate('fos_user_registration_confirmed');
-                    $response = new RedirectResponse($url);
-                }
-
-                $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
-
-                return $response;
+            if (null === $response = $event->getResponse()) {
+                $url = $this->generateUrl('fos_user_registration_confirmed');
+                $response = new RedirectResponse($url);
             }
+
+            $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
+            return $response;
         }
 
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
+        return $this->render('FOSUserBundle:Registration:register.html.twig', array(
             'form' => $form->createView(),
         ));
     }
@@ -120,15 +116,15 @@ class RegistrationController extends Controller
      */
     public function checkEmailAction()
     {
-        $email = $this->container->get('session')->get('fos_user_send_confirmation_email/email');
-        $this->container->get('session')->remove('fos_user_send_confirmation_email/email');
-        $user = $this->container->get('fos_user.user_manager')->findUserByEmail($email);
-		/*
+        $email = $this->get('session')->get('fos_user_send_confirmation_email/email');
+        $this->get('session')->remove('fos_user_send_confirmation_email/email');
+        $user = $this->get('fos_user.user_manager')->findUserByEmail($email);
+
         if (null === $user) {
             throw new NotFoundHttpException(sprintf('The user with email "%s" does not exist', $email));
         }
-		*/
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:checkEmail.html.'.$this->getEngine(), array(
+
+        return $this->render('FOSUserBundle:Registration:checkEmail.html.twig', array(
             'user' => $user,
         ));
     }
@@ -139,18 +135,16 @@ class RegistrationController extends Controller
     public function confirmAction(Request $request, $token)
     {
         /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
-        $userManager = $this->container->get('fos_user.user_manager');
+        $userManager = $this->get('fos_user.user_manager');
 
         $user = $userManager->findUserByConfirmationToken($token);
 
         if (null === $user) {
-            $url = $this->container->get('router')->generate('ui_company');
-            $response = new RedirectResponse($url);
-			return $response;
+            throw new NotFoundHttpException(sprintf('The user with confirmation token "%s" does not exist', $token));
         }
 
         /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
-        $dispatcher = $this->container->get('event_dispatcher');
+        $dispatcher = $this->get('event_dispatcher');
 
         $user->setConfirmationToken(null);
         $user->setEnabled(true);
@@ -161,7 +155,7 @@ class RegistrationController extends Controller
         $userManager->updateUser($user);
 
         if (null === $response = $event->getResponse()) {
-            $url = $this->container->get('router')->generate('ui_company');
+            $url = $this->generateUrl('fos_user_registration_confirmed');
             $response = new RedirectResponse($url);
         }
 
@@ -175,18 +169,14 @@ class RegistrationController extends Controller
      */
     public function confirmedAction()
     {
-        $user = $this->container->get('security.context')->getToken()->getUser();
+        $user = $this->getUser();
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
 
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:confirmed.html.'.$this->getEngine(), array(
+        return $this->render('FOSUserBundle:Registration:confirmed.html.twig', array(
             'user' => $user,
         ));
     }
-
-    protected function getEngine()
-    {
-        return $this->container->getParameter('fos_user.template.engine');
-    }
 }
+
